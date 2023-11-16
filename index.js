d124a21512c3454d0c81c8996324edf95714573e
Dana Holubová <danmich@email.cz>
2023-11-16 20:25:13 +0100

    Dokončení druhé části úkolu.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
 const goodbye = (userName) => {
-    document.querySelector(".email__closing").textContent = `Na shledanou ${userName}`
+    document.querySelector(".email__closing").textContent = `S pozdravem ${userName}`
 }
 
-goodbye('Pavel Ovesný')
+goodbye("Pavel Ovesný")
+
+const fillSubject = (subject) => {
+    document.querySelector(".email__subject").textContent = subject
+}
+
+fillSubject("Nová zpráva")
+
+const fillBody = (body, name) => {
+    document.querySelector(".email__body").textContent = body
+    goodbye(name)
+}
+
+fillBody("Toto je tělo emailu.", "Dana H.")
+
+
